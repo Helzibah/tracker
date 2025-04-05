0060de0f04e40afed417c7dd7b2f85122e23c94e
Helen Briolat <helen.briolat@gmail.com>
2025-04-05 20:34:13 +0100

    update times to elide duplicate start info

--- src/app/timeline/line/line.component.ts
+++ src/app/timeline/line/line.component.ts
@@ -18,8 +18,39 @@ export class LineComponent {
   @Input({required: true}) timezones !: string[];
   @Input() class: string = '';
 
-  format = 'yyyy-MM-dd HH:mm ZZZZ';
+  format = DateTime.DATETIME_FULL;
 
   ngOnInit() {
+  }
+
+  getDateStrings(datetime: DateTime): string[] {
+    var formatted = [];
+    var substr = '';
+    var length = 0;
+    for (var zone of this.timezones) {
+      var dt;
+      if (zone == '_') {
+        dt = datetime.toLocaleString(this.format);
+      } else {
+        dt = datetime.setZone(zone).toLocaleString(this.format);
+      }
+
+      if (substr == '') {
+        formatted.push(dt);
+
+        // todo: this won't work in any other locale, but it won't NOT work
+        var i = dt.indexOf(' at ');
+        substr = dt.substring(0, i);
+        length = substr.length;
+      } else {
+        if (dt.startsWith(substr)) {
+          dt = dt.substring(length);
+        }
+
+        formatted.push(dt);
+      }
+    }
+
+    return formatted;
   }
 }
